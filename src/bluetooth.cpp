60e222e6ce037ddc6cbde6269041ca5707a76afa
Konrad Klimczak <konrad.klimczak.4@gmail.com>
2022-07-10 00:06:59 +0200

    feat(bluetooth): replace bluetooth classic with BLE

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -1,16 +1,33 @@
 #include <Arduino.h>
-#include <BluetoothSerial.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
 #include "bluetooth.h"
 
+#define SERVICE_UUID "58304f18-ffd1-11ec-b939-0242ac120002"
+#define CHARACTERISTIC_UUID "583050d0-ffd1-11ec-b939-0242ac120002"
+
 namespace Bluetooth {
-    BluetoothSerial SerialBT;
     
     void setup() {
-        SerialBT.begin("LapTimer");
+        BLEDevice::init("SLT");
+        BLEServer *pServer = BLEDevice::createServer();
+        BLEService *pService = pServer->createService(SERVICE_UUID);
+        BLECharacteristic *pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+        pCharacteristic->setValue("Booting...");
+        pService->start();
+
+        BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+        pAdvertising->addServiceUUID(SERVICE_UUID);
+        pAdvertising->setScanResponse(true);
+        pAdvertising->setMinPreferred(0x06);
+        pAdvertising->setMinPreferred(0x12);
+        BLEDevice::startAdvertising();
     }
 
     void update() {
-        SerialBT.println("test");
+        
     }
 }
